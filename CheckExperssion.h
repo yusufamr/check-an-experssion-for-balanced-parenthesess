23d95bb52e0fe589e26fe76d7425d8d55d394ed0
yusufamr <50525245+yusufamr@users.noreply.github.com>
2021-08-18 15:13:30 +0200

    Add files via upload

---
+++ CheckExperssion.h
@@ -0,0 +1,7 @@
+#ifndef CHECKEXPERSSION_H_INCLUDED
+#define CHECKEXPERSSION_H_INCLUDED
+#include "stack.h"
+sint8 Check_experssion(uint8 *Data);
+uint8 isMatching(uint8 char1, uint8 char2);
+
+#endif // CHECKEXPERSSION_H_INCLUDED
