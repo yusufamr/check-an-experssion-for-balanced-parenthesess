2f18fc8bd472896e65ff35a0db6c788b750a3f85
yusufamr <50525245+yusufamr@users.noreply.github.com>
2021-09-06 10:24:02 +0200

    Delete CheckExperssion.h

--- CheckExperssion.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHECKEXPERSSION_H_INCLUDED
-#define CHECKEXPERSSION_H_INCLUDED
-#include "stack.h"
-sint8 Check_experssion(uint8 *Data);
-uint8 isMatching(uint8 char1, uint8 char2);
-
-#endif // CHECKEXPERSSION_H_INCLUDED
